f04dcb4ea034ad700a085209c0a960c68e942c86
carl <carl@debian-BULLSEYE-live-builder-AMD64>
2025-03-23 16:45:53 +0100

    updated UDP handling

---
+++ tests/testudp6.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+import threading
+import time
+
+portFrom = int(sys.argv[1])
+toIP   = sys.argv[2]
+portTo = int(sys.argv[3])
+
+bytespersec   = int(sys.argv[4])
+s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+s.bind(("", portFrom))
+
+def send():
+    while True:
+        s.sendto(b"a"*int(bytespersec/10+0.5), (toIP, portTo))
+        time.sleep(0.1)
+
+t = threading.Thread(daemon=True, target=send)
+t.start()
+
+while True:
+    data = s.recvfrom(1024)
+
